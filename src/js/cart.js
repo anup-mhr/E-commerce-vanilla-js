dc76c3800604e7b0198e594c272da26906396809
A-nup <anupmhrzn16@gmail.com>
2023-10-29 17:31:37 +0545

    finished cart process

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,5 +1,12 @@
 // let cart = JSON.parse(localStorage.getItem('cart')) || []
 
+const cartDOM = document.querySelector('.cart-inner')
+let products = JSON.parse(localStorage.getItem('products')) || getProducts()
+const summaryTotal = document.querySelector('#summaryTotal')
+const summaryContainer = document.querySelector('#summaryContainer')
+const discount = document.querySelector('#discount')
+const checkoutBtn = document.querySelector('#checkoutBtn')
+
 function addCartItem(e) {
     console.log('adding')
     let selectedItem = e.target;
@@ -21,3 +28,84 @@ function addCartItem(e) {
 function cartStorage(cart) {
     localStorage.setItem('cart', JSON.stringify(cart))
 }
+
+function generateCart() {
+    cartDOM.innerHTML = cart.map(item => {
+        let search = products.find(x => x.id === item.id);
+        let { id, title, price, discountPercentage } = search
+        let discountedPrice = Math.floor(price - (price * discountPercentage) / 100)
+
+        return `<ul data-id='${id}'>
+        <li>${title}</li>
+        <li>$ ${discountedPrice}</li>
+        <li><i onclick='decrement(${id})' class="bi bi-chevron-down"></i>${item.quantity}<i onclick="increment(${id})" class="bi bi-chevron-up"></i></li>
+        <li>$ ${item.quantity * discountedPrice}</li>
+        <li><button class="btn" onclick='removeCart(${id})'>remove</button></li>
+    </ul>`
+    }).join('')
+}
+
+function removeCart(id) {
+    let selectedItem = document.querySelector(`[data-id='${id}']`)
+    cart = cart.filter(item => item.id !== id)
+    cartStorage(cart)
+    selectedItem.remove()
+    alert('item removed')
+}
+
+function increment(id) {
+    let search = cart.find((x) => x.id === id)
+    if (search) search.quantity += 1;
+    cartStorage(cart);
+    calculateTotalItems()
+    generateCart()
+    summaryCalculation()
+}
+
+function decrement(id) {
+    let search = cart.find((x) => x.id === id)
+    if (search === undefined) return
+    if (search.quantity === 0) return
+    else {
+        search.quantity -= 1;
+    }
+    cart = cart.filter(x => x.quantity !== 0)
+    cartStorage(cart);
+    generateCart()
+    calculateTotalItems()
+    summaryCalculation()
+}
+
+function summaryCalculation() {
+    let qty = ''
+    let price = ''
+    let discountPercentage = ''
+    let totalPrice = cart.map(item => {
+        qty = item.quantity
+        let search = products.find(x => x.id === item.id)
+        price = search.price
+        discountPercentage = search.discountPercentage
+        let discountedPrice = Math.floor(price - (price * discountPercentage) / 100)
+        return qty * discountedPrice
+    }).reduce((x, y) => x + y)
+    summaryTotal.innerHTML = totalPrice
+    summaryContainer.innerHTML = price * qty
+    discount.innerHTML = Math.floor(totalPrice * discountPercentage / 100)
+}
+
+checkoutBtn.addEventListener('click', () => {
+    let users = JSON.parse(localStorage.getItem('users')) || []
+    if (!users) {
+        alert('login please')
+        return
+    }
+    let isAuth = users.find(user=> user.isAuth===true);
+    if(!isAuth){
+        alert('login please')
+        return
+    }
+    window.location.replace('/pages/checkout.html')
+})
+
+generateCart()
+summaryCalculation()
---
+++ src/js/checkout.js
@@ -0,0 +1,17 @@
+let totalBill = document.querySelector('#total-bill')
+
+let products = JSON.parse(localStorage.getItem('products')) || getProducts()
+
+
+function summaryCalculation(){
+    totalBill.innerHTML = cart.map(item=>{
+        qty = item.quantity
+        let search = products.find(x=> x.id===item.id)
+        price = search.price
+        discountPercentage = search.discountPercentage
+        let discountedPrice = Math.floor(price - (price * discountPercentage) / 100)
+        return qty * discountedPrice
+    }).reduce((x,y)=>x+y)
+}
+
+summaryCalculation()
